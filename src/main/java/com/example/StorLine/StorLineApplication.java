27558b6d49822d170a8b5922f125322d6b0496d8
Javier-Cs <krchipullajavier@gmail.com>
2025-09-08 23:06:44 -0500

    creación de proyecto StorLine

---
+++ src/main/java/com/example/StorLine/StorLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.StorLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StorLine/StorLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StorLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
